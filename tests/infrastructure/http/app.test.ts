6e3a231a80132289d82ab659b1f91b3a47e2f6b0
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-10-27 18:27:24 -0500

    agregar comentario en test de parte 7

--- tests/infrastructure/http/app.test.ts
+++ tests/infrastructure/http/app.test.ts
@@ -5,16 +5,19 @@ import { CreateBooking } from "../../../src/application/CreateBooking";
 import { createApp } from "../../../src/infrastructure/http/app";
 
 test("Deberia invocar caso de uso de creación de reserva", async () => {
+  // Arrange
   const usecase = mock<CreateBooking>();
   const scope = container.createChildContainer();
   scope.registerInstance(CreateBooking, usecase);
   const app = createApp(scope);
 
+  // Act
   const response = await request(app).post("/booking").send({
     date: "2021-01-01",
     email: "test@test.com",
   });
 
+  // Assert
   expect(usecase.execute).toHaveBeenCalledWith({
     date: "2021-01-01",
     email: "test@test.com",
